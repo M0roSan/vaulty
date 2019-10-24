b0429d0378d4a99b4040715630f8c14b42d2aff5
MasahiroYoshida <Masahiro.Yoshida.SE@gmail.com>
2019-10-24 11:24:12 -0500

    file baked

--- cmd/demo.go
+++ cmd/demo.go
@@ -7,12 +7,30 @@ import (
 )
 
 func main() {
-	v := secret.Memory("my-fake-key")
+	v := secret.File("my-fake-key", ".secrets")
 	err := v.Set("demo_key", "some value")
 	if err != nil {
 		panic(err)
 	}
+	err = v.Set("demo_key2", "some crazy value")
+	if err != nil {
+		panic(err)
+	}
+	err = v.Set("demo_key3", "some value 1234567898765432")
+	if err != nil {
+		panic(err)
+	}
 	plain, err := v.Get("demo_key")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(plain)
+	plain, err = v.Get("demo_key2")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(plain)
+	plain, err = v.Get("demo_key3")
 	if err != nil {
 		panic(err)
 	}
